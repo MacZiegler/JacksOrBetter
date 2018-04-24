06ec62a22494a5cfb825ec6e1c055d13351b503e
TobyZiegler <tobyziegler@tobyziegler.com>
2018-04-24 16:20:24 -0500

    add function outline for game process

--- jacksorBetter.js
+++ jacksorBetter.js
@@ -1,5 +1,24 @@
-angular.module('jacksorBetterApp', [])
-.controller('jacksorBetterController', function () {
+angular.module('jacksorBetterApp', []);
+
+function GameController(){
+    var game = this;
+ 
+    game.init = function () {
+        game.started = false;
+    };
+ 
+    game.start = function () {
+        game.started = true;
+    };
+ 
+    game.end = function () {
+        game.started = false;
+    };
+ 
+    game.init();
+};
+
+function DeckController() {
     var deck = [];
     deck.suits = [
         "null",
@@ -24,4 +43,15 @@ angular.module('jacksorBetterApp', [])
         "queen",
         "king"
     ];
-})
+};
+
+function shuffle (){};
+
+function deal (){};
+
+function hand (){};
+
+function bet (){};
+
+function score (){};
+// calculate payout as bet * scale unless 5 coin royal flush, then 4000
